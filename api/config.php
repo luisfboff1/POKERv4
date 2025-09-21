8be4b6d5b8e67c66441ad6f43fd705ba54ad662c
luisfboff1 <luisfboff@gmail.com>
2025-09-21 11:08:20 -0300

    debug: Adicionar arquivos de teste e corrigir headers CORS

--- api/config.php
+++ api/config.php
@@ -17,7 +17,7 @@ if (file_exists(__DIR__ . '/.env')) {
 // Headers CORS
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
